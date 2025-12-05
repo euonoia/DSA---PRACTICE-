57eea72b7eeaa1a825511c703bd10d5baf00b757
Ken Española <revas4192@gmail.com>
2025-12-05 12:37:49 +0800

    initial commit

--- primitivesvsreferences/manualdeepclone.js
+++ primitivesvsreferences/manualdeepclone.js
@@ -1,22 +1,23 @@
 export function solve(){
     function deepclone(value){
-        if(typeof value !== "object" || value == null ){
+        if (typeof value !== "object" || value==null){
             return value;
         }
-        if(Array.isArray(value)){
+        if (Array.isArray(value)){
             return value.map((item)=>deepclone(item));
         }
         const obj = {};
-        for (let key in value){
+         for (let key in value){
             obj[key] = deepclone(value[key]);
-        }
-        return obj;
+         }
+         return obj;
     }
-    const original = {name:"ken", skills:["js","react"]};
-    const copy = deepclone(original);
+    const data = {name: "ken", skills:["react","java"]};
+    const clone = deepclone(data);
+    
+    clone.skills.push("css");
 
-    copy.skills.push("node");
+    console.log(data.skills);
 
-    console.log(original.skills); // [js, react]
-    return;
+    return solve;
 }
