79282de7698a742842c18da5f51fcfc119e4239e
Ken Española <revas4192@gmail.com>
2025-12-05 14:04:32 +0800

    Update Arrays&Objects.js

--- Objects&Arrays/Arrays&Objects.js
+++ Objects&Arrays/Arrays&Objects.js
@@ -1,20 +1,20 @@
-export function solve() {
-  function twoSumSorted(arr, target) {
-  let left = 0;
-  let right = arr.length - 1;
+export function solve(){
+    function twoSumSorted(arr,target){
+        let left = 0;
+        let right = arr.length - 1;
 
-  while (left < right) {
-    const sum = arr[left] + arr[right];
+        while (left < right ){
+            const sum = arr[left] + arr[right];
 
-    if (sum === target) return [arr[left], arr[right]];
-    if (sum < target) left++;
-    else right--;
-  }
+            if (sum === target) 
+                return [arr[left],arr[right]];
+            
+            if (sum < target ) left ++;
+            else right--;
+        }
+        return null;
+    }
 
-  return null;
-}
+    console.log(twoSumSorted([-50, -40, -30, -20, -10, 0, 5, 10],-25));
 
- console.log(twoSumSorted([1, 4, 5, 9, 11, 15], 20));
-
-  return { twoSumSorted };
 }
