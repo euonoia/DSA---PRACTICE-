54de3757011a4ef3e03637837b7db81dd17a5a33
Ken Española <revas4192@gmail.com>
2025-12-08 17:29:10 +0800

    refreshen up

--- primitivesvsreferences/manualdeepclone.js
+++ primitivesvsreferences/manualdeepclone.js
@@ -1,23 +1,21 @@
 export function solve(){
-    function deepclone(value){
-        if (typeof value !== "object" || value==null){
+    function deepClone(value){
+        if(typeof value !== 'object' || value == null){
             return value;
         }
         if (Array.isArray(value)){
-            return value.map((item)=>deepclone(item));
+            return value.map((item)=>deepClone(item));
         }
-        const obj = {};
-         for (let key in value){
-            obj[key] = deepclone(value[key]);
-         }
-         return obj;
+        const obj ={};
+        for (let key in value){
+            obj[key] = deepClone(value[key]);
+        }
+        return obj;
     }
-    const data = {name: "ken", skills:["react","java"]};
-    const clone = deepclone(data);
-    
-    clone.skills.push("css");
+    const data = {name:"john", skills:["js","java"]};
+    const clone = deepClone(data);
+
+    clone.skills.push("python");
 
     console.log(data.skills);
-
-    return solve;
 }
