eb4b603882f430882cbc3ad5034d657bf12b057c
Ken Española <revas4192@gmail.com>
2025-12-08 17:18:18 +0800

    initial commit

--- Objects&Arrays/Arrays&Objects.js
+++ Objects&Arrays/Arrays&Objects.js
@@ -1,20 +1,17 @@
 export function solve(){
-    function twoSumSorted(arr,target){
+    function twoSumSorted(arr, target){
         let left = 0;
         let right = arr.length - 1;
-
-        while (left < right ){
-            const sum = arr[left] + arr[right];
-
-            if (sum === target) 
+        while(left<right){
+            const sum = arr[left] + arr [right];
+            if (sum === target){
                 return [arr[left],arr[right]];
-            
-            if (sum < target ) left ++;
-            else right--;
+            }
+            else if (sum<target) left++;
+            else right --;
         }
         return null;
     }
-
-    console.log(twoSumSorted([-50, -40, -30, -20, -10, 0, 5, 10],-25));
-
+    console.log(twoSumSorted([1,2,3,4,6],6));
+    return;
 }
